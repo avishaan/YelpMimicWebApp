ea356d7e114f890c19e006d35ce8e88d3fb027ba
Avishaan <avishaan@codeHatcher.com>
2014-09-09 16:15:27 -0700

    updating restaurants routes

--- api/controllers/restaurants.js
+++ api/controllers/restaurants.js
@@ -1,1 +1,32 @@
 'use strict'
+
+var request = require('request');
+var async = require('async');
+
+var config = require('../../config/config.js');
+
+module.exports = {
+  getRestaurants: getRestaurants,
+  getRestaurantById: getRestaurantById
+};
+
+function getRestaurants(req, res){
+  request(config.UG + '/restaurants', function(err, response, body){
+    if (err){
+      res.send(err);
+    } else {
+      res.send(body);
+    }
+  });
+}
+
+function getRestaurantById(req, res){
+  var restId = req.swagger.params.id.value;
+  request(config.UG + '/restaurants?ql=restID=' + restID, function(err, response, body){
+    if (err){
+      res.send(err);
+    } else {
+      res.send(body);
+    }
+  });
+}
