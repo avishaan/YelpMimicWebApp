cc37b6a664edcab4f7353eb3b38549aa4669b01c
Avishaan <avishaan@codeHatcher.com>
2014-09-09 16:54:57 -0700

    add reviews controller

---
+++ api/controllers/reviews.js
@@ -0,0 +1,20 @@
+function getReviews (req, res) {
+	var qs = "";
+	if (req.swagger.params.restID.value) {
+		var restID = req.swagger.params.restID.value;
+		qs = "?ql=restID=" + restID;
+	} else if (req.swagger.params.rating.value) {
+		var rating = req.swagger.params.rating.value;
+		qs = "?ql=rating=" + rating;
+	} else if (req.swagger.params.reviewer.value) {
+		var reviewer = req.swagger.params.reviewer.value;
+		qs = "?ql=reviewer=" + reviewer;
+	}
+	request(config.UG + '/reviews' + qs, function(error, response, body) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.send(body);
+        }
+    });
+}
